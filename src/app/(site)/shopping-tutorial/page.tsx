a33b385002357746aa2ce0f3a369fd1ff19bbc4c
apsmanda <amandasulistyorini@gmail.com>
2025-07-20 23:27:21 +0700

    Update page.tsx

--- src/app/(site)/shopping-tutorial/page.tsx
+++ src/app/(site)/shopping-tutorial/page.tsx
@@ -9,7 +9,7 @@ const shoppingtutorial = () => {
                     <li>Pilih produk favorit Anda dari halaman koleksi kami.</li>
                     <li>Pada halaman detail produk, pilih ukuran yang sesuai dan klik tombol "Tambah ke Keranjang".</li>
                     <li>Setelah selesai memilih produk, klik ikon keranjang belanja di pojok kanan atas.</li>
-                    <li>Periksa kembali pesanan Anda, lalu klik "Checkout" untuk melanjutkan ke pembayaran.</li>
+                    <li>Periksa kembali pesanan Anda, lalu klik Checkout untuk melanjutkan ke pembayaran.</li>
                     <li>Isi alamat pengiriman dan pilih metode pembayaran yang Anda inginkan.</li>
                     <li>Selesaikan pembayaran dan tunggu konfirmasi pesanan dari kami melalui email.</li>
                 </ol>
